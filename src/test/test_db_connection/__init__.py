93e8e24e53c04650c76733180e600cc7e8ad58c2
PDT <darkflayer@gmail.com>
2020-12-02 07:11:49 +0100

    added test database setup

--- src/test/test_db_connection/__init__.py
+++ src/test/test_db_connection/__init__.py
@@ -4,5 +4,21 @@
 
 This is the package for database tests.
 """
+import os
 
-TEST_DB_PATH: str = "../../../res/inloom_qt/test_database.db"
+from db_connection.db_connection import SqliteConnection
+
+TEST_DB_PATH: str = "../../../res/inloomqt-res/test_database.db"
+
+
+# noinspection PyProtectedMember
+def init_test_sqlite_connection():
+    """Create an Sqlite Connection for test purposes."""
+
+    if SqliteConnection._instance:
+        del SqliteConnection._instance
+    if os.path.exists(TEST_DB_PATH):
+        os.remove(TEST_DB_PATH)
+    with open(TEST_DB_PATH, 'w+'):
+        pass
+    SqliteConnection(TEST_DB_PATH)
