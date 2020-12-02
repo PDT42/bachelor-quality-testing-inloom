ba0d0075352efb0578106c8f1ed4a794abfc8b30
PDT <darkflayer@gmail.com>
2020-12-02 07:11:32 +0100

    added doc

---
+++ src/managers/__init__.py
@@ -0,0 +1,8 @@
+"""
+:Author: Paul Erlenwein
+:Since: 2020/11/20
+
+This is the package for all manager classes of the project. Each datatype,
+that must be stored in the database gets it's own manager class. The manager
+will provide all the interface operations required for the datatype.
+"""
