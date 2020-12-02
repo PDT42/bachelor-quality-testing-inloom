fc5a8e6a20466c6af5211bbab45b54445e25866a
PDT <darkflayer@gmail.com>
2020-12-02 07:10:00 +0100

    added sqlite_convert function, which converts literals to sqlite compatible types

--- src/db_connection/__init__.py
+++ src/db_connection/__init__.py
@@ -5,4 +5,37 @@
 This is the db_connection package.
 """
 
-DB_PATH: str = "../res/inloom_qt/database.db"
+from typing import Any
+from uuid import UUID
+
+from data_types.evaluation import EvaluationType
+from data_types.result import ResultCategory
+
+DB_PATH: str = "../res/inloomqt-res/database.db"
+VERBOSITY = 5
+
+
+def sqlite_convert(item: Any) -> str:
+    """Make an item insertable."""
+
+    # Converting Primitives
+    if isinstance(item, str):
+        return f"\"{item}\""
+    elif item is None:
+        return "null"
+    elif isinstance(item, bool):
+        return str(int(item))
+
+    # Converting complex types
+    elif isinstance(item, UUID):
+        return f"\"{str(item)}\""
+
+    # Converting Enums
+    elif isinstance(item, ResultCategory):
+        return f"\"{item.value}\""
+    elif isinstance(item, EvaluationType):
+        return f"\"{item.value}\""
+
+    # Hoping for the best
+    else:
+        return str(item)
