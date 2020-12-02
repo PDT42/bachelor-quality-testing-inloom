8f01e2ec3336a82c49641d54de7583aa23021684
PDT <darkflayer@gmail.com>
2020-12-02 07:10:26 +0100

    Using sqlite_convert function

--- src/db_connection/filter.py
+++ src/db_connection/filter.py
@@ -6,12 +6,32 @@ This is the module that contains the ``Filter``.
 """
 
 from dataclasses import dataclass
+from enum import Enum
+from typing import Any
 
+from db_connection import sqlite_convert
 from db_connection.db_column import DbColumn
+
+
+class FilterOperation(Enum):
+    """Available Filter Operations."""
+
+    EQUALS = '='
 
 
 @dataclass
 class Filter:
     """This is the representation of a db ``Query`` filter."""
 
-    filter_attribute: DbColumn
+    column: DbColumn
+    operation: FilterOperation
+    value: Any
+
+    def resolve(self, conversion_function: callable = sqlite_convert):
+        """Resolve a filter."""
+
+        rep: str = f"\"{self.column.column_name}\""
+        rep += self.operation.value
+        rep += conversion_function(self.value)
+
+        return rep
