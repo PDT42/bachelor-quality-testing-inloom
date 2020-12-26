4b752198d4938f93e9cc3f19131178b3177f0c5c
PDT <darkflayer@gmail.com>
2020-12-26 07:13:04 +0100

    updated servers

--- src/servers/evaluation_server.py
+++ src/servers/evaluation_server.py
@@ -4,15 +4,19 @@
 
 This is the module for the ``EvalServer``.
 """
-
+import json
 import os
+from typing import Any, Dict, List
+from uuid import uuid4
 
-from flask import Flask, make_response, request
+from flask import Flask, jsonify, make_response, request, Response
 from werkzeug.utils import secure_filename
 
+from data_types.evaluation import Evaluation, ManEval
+from data_types.test_data_set import TestDataSet
 from managers.evaluation_manager import EvalManager
 from managers.testdata_manager import TDManager
-from servers import AUTO_EVAL_PATH
+from servers import AUTO_EVAL_FORMATS, AUTO_EVAL_PATH, MAN_EVAL_FORMATS, MAN_EVAL_PATH
 from xml_adapter import XMLAdapter, XMLAdapterResult
 
 
@@ -36,15 +40,6 @@ class EvalServer:
     # Variables
     _initialized: bool = False
 
-    td_manager: TDManager = None
-    eval_manager: EvalManager = None
-
-    def __init__(self):
-        """Create a new ``EvalServer``."""
-
-        self.td_manager = TDManager.get()
-        self.eval_manager = EvalManager.get()
-
     @staticmethod
     def register_routes(app: Flask):
         """Register the routes this server provides in the ``app``."""
@@ -53,44 +48,144 @@ class EvalServer:
             raise SystemError()
 
         app.add_url_rule(
-            rule='/eval/auto/upload',
-            endpoint='upload-auto-eval',
-            view_func=EvalServer._post_upload_auto_eval,
+            rule='/eval/register/man',
+            endpoint='register-man-eval',
+            view_func=EvalServer._post_register_man_eval,
+            methods=['POST', 'OPTIONS']
+        )
+
+        app.add_url_rule(
+            rule='/eval/register/auto',
+            endpoint='register-auto-eval',
+            view_func=EvalServer._post_register_auto_eval,
             methods=['POST']
+        )
+
+        app.add_url_rule(
+            rule='/eval',
+            endpoint='get-alle-evaluations',
+            view_func=EvalServer._get_all_evals,
+            methods=['GET']
+        )
+
+        app.add_url_rule(
+            rule='/eval:<string:evaluation_id>',
+            endpoint='get-evaluation-by-id',
+            view_func=EvalServer._get_eval_by_id,
+            methods=['GET']
         )
 
         print("Initialized EvalServer ...")
         EvalServer.get()._initialized = True
 
     @staticmethod
-    def _post_upload_auto_eval():
-        """Process POST calls to [/eval/auto/upload]"""
+    def _post_register_man_eval():
+        """Process POST calls tp [/eval/register]."""
 
+        if request.method == 'POST':
+
+            new_eval_data: Dict[str, Any] = json.loads(request.data)
+
+            student_id: str = new_eval_data.get('student_id', False)
+            exercise_id: str = new_eval_data.get('exercise_id', False)
+            expert_solution_id: str = new_eval_data.get('expert_solution_id', False)
+
+            if not all([student_id, exercise_id, expert_solution_id]):
+                raise KeyError("MISSING KEY ERROR!")
+
+            test_data_set: TestDataSet = TDManager.get().get_student_tds(
+                expert_solution_id=expert_solution_id,
+                student_id=student_id,
+                exercise_id=exercise_id)
+
+            if not test_data_set:
+
+                # TODO: Create new TDS and store it in db
+                test_data_set = TestDataSet(
+                    exercise_id=exercise_id,
+                    expert_solution_id=expert_solution_id,
+                    student_id=student_id,
+                    meta_model_type=new_eval_data.get('expert_solution_id'),
+                    max_points=new_eval_data.get('max_points'))
+
+                TDManager.get().insert_test_data_sets(test_data_set)
+
+            man_eval: ManEval = ManEval.from_dict(new_eval_data, test_data_set.test_data_set_id)
+            EvalManager.get().insert_evaluation(man_eval)
+
+        return Response()
+
+    @staticmethod
+    def _post_register_auto_eval():
+        """Process POST calls to [/eval/auto/upload]."""
+
+        # Get the file from the received request
         auto_eval_file = request.files['file']
         auto_eval_file_name: str = auto_eval_file.filename
 
+        # Create a response to return later
         response = make_response()
-        response.headers.add('Access-Control-Allow-Origin', '*')
 
-        if auto_eval_file_name == '' or auto_eval_file_name.split('.')[-1] != 'xml':
-            response.headers.add('status', '400')
+        # Check whether the uploaded file has an allowed format
+        if auto_eval_file_name == '' \
+                or auto_eval_file_name.split('.')[-1].lower() not in AUTO_EVAL_FORMATS:
+            response.status_code = 301
             return response
 
         # TODO: Identify evaluation file ...
 
+        # Get a secure filename from the files name
         auto_eval_file_name = secure_filename(auto_eval_file_name)
         auto_eval_file_path = os.path.join(AUTO_EVAL_PATH, auto_eval_file_name)
         auto_eval_file.save(auto_eval_file_path)
 
+        # Get the data from the xml file ...
         xml_adapter_res: XMLAdapterResult = XMLAdapter.eval_from_xml(auto_eval_file_path)
-        EvalServer.get().td_manager.register_evaluation(
-            evaluation=xml_adapter_res.evaluation,
-            meta_model_type=xml_adapter_res.meta_model_type,
-            mcs_identifier=xml_adapter_res.mcs_id,
-            mcs_version=xml_adapter_res.mcs_version)
+        auto_eval: Evaluation = xml_adapter_res.evaluation
+
+        test_data_set: TestDataSet = TDManager.get().get_student_tds(
+            expert_solution_id=auto_eval.expert_solution_id,
+            student_id=auto_eval.student_id,
+            exercise_id=auto_eval.exercise_id)
+
+        if not test_data_set:
+            test_data_set = TestDataSet(
+                exercise_id=auto_eval.exercise_id,
+                expert_solution_id=auto_eval.expert_solution_id,
+                student_id=auto_eval.student_id,
+                meta_model_type=auto_eval.meta_model_type,
+                max_points=auto_eval.max_points)
+            TDManager.get().insert_test_data_sets(test_data_set)
+
+        auto_eval.test_data_set_id = test_data_set.test_data_set_id
+        auto_eval.file_path = auto_eval_file_path
+        EvalManager.get().insert_evaluation(auto_eval)
+
+        # Setting the status of the response to 200
+        response.status_code = 200
 
         return response
 
     @staticmethod
-    def _get_eval_by_id():
-        """Handle """
+    def _post_upload_man_eval():
+        """Handle POST requests to [/eval/man/upload]"""
+
+        # TODO
+        pass
+
+    @staticmethod
+    def _get_eval_by_id(evaluation_id: str):
+        """Handle GET requests to [/eval:{evaluation_id}]."""
+
+        evaluation: Evaluation = EvalManager.get()\
+            .get_one_by_id(evaluation_id=evaluation_id)
+
+        return make_response(jsonify(evaluation))
+
+    @staticmethod
+    def _get_all_evals():
+        """Handle GET requests to [/eval]."""
+
+        evaluations = EvalManager.get().get_all_evaluations()
+
+        return make_response(jsonify([e.__dict__ for e in evaluations]))
--- src/servers/test_data_server.py
+++ src/servers/test_data_server.py
@@ -8,7 +8,7 @@ from typing import List
 
 from flask import jsonify, make_response
 
-from data_types.testdataset import TestDataSet
+from data_types.test_data_set import TestDataSet
 from managers.testdata_manager import TDManager
 
 
@@ -33,13 +33,6 @@ class TDServer:
     # Variables
     _initialized: bool = False
 
-    td_manager: TDManager = None
-
-    def __init__(self):
-        """Create a new ``TDServer``."""
-
-        self.td_manager = TDManager.get()
-
     @staticmethod
     def register_routes(app):
         """Register the routes this server provides in the ``app``."""
@@ -54,15 +47,14 @@ class TDServer:
             methods=['GET']
         )
 
-        print("Initialized EvalServer ...")
+        print("Initialized TestDataServer ...")
         TDServer.get()._initialized = True
 
     @staticmethod
     def _get_all_test_data_sets():
         """Process GET calls to [/testdatasets]"""
 
-        test_data_sets: List[TestDataSet] = TDServer.get().td_manager.get_all_test_data_sets()
+        test_data_sets: List[TestDataSet] = TDManager() \
+            .get_all_test_data_sets()
 
-        response = make_response(jsonify(test_data_sets))
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+        return make_response(jsonify(test_data_sets))
